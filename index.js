378f84e6f8e9db375d03a1d23f9b83a8d5de3c56
Sylwester Wroblewski <swrobl@proton.me>
2026-07-10 19:17:04 +0200

    Dokerfile and other

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 
 const app = express();
-const PORT = 3000;
+const PORT = process?.env?.PORT || 3000;
+
 
 app.get('/', (req, res) => {
     res.send('Hello World');
+});
+
+
+app.get('/env', (req, res) => {
+    res.send(`
+  <html>
+    <body>
+      <pre>${JSON.stringify(process.env, null, 2)}</pre>
+    </body>
+  </html>
+`);
 });
 
 app.listen(PORT, () => {
